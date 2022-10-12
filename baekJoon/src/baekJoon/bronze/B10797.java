7a89a2dbfbe05234730d3d6c31dfa7c09d4c3cb7
booimym <booimym@naver.com>
2022-10-13 06:30:19 +0900

    10/13

---
+++ baekJoon/src/baekJoon/bronze/B10797.java
@@ -0,0 +1,37 @@
+package baekJoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B10797 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = br.readLine();
+
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		String[] tokenArr = new String[st.countTokens()];
+		int count = 0;
+		int countTokens = st.countTokens();
+		//오 왜인지 모르겠지만,,,!!!!!!! int형에 집어넣으면 다 출력된다
+		//바로 st.countTokens까지 돌린다고 하면 중간만 출력됨..args.
+		for(int i = 0; i< countTokens;i++) {
+			
+			tokenArr[i] = st.nextToken(); // tokenArr에 모든 숫자들을 담았다.
+//			System.out.println(tokenArr[i]);
+			if (tokenArr[i].equals(input)) {
+				count++;
+			}
+		} 
+		
+		System.out.println(count);
+		
+		
+	}
+
+}
---
+++ baekJoon/src/baekJoon/bronze/B9093.java
@@ -0,0 +1,37 @@
+package baekJoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B9093 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<num ;i++) {
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int countTokens = st.countTokens();
+			String[] arr = new String[countTokens];
+			for(int j= 0 ; j<countTokens ; j++) {
+				StringBuilder sb2 = new StringBuilder(st.nextToken());
+				
+				arr[j]=sb2.reverse().toString();
+				sb.append(arr[j]+" ");
+			}
+		sb.append("\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+}
