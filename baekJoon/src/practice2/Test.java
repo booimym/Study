d5c398551bdf561f3903be510ba474eafa357a90
booimym <booimym@naver.com>
2023-03-04 23:19:37 +0900

    .

--- baekJoon/src/practice2/Test.java
+++ baekJoon/src/practice2/Test.java
@@ -1,13 +1,98 @@
 package practice2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Points implements Comparable<Points> {
+    private int x;
+    private int y;
+
+    public Points(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public int compareTo(Points p) {
+        if(this.x > p.getX()) {
+        	System.out.println("1이래");
+            return 1;
+        }
+        else if(this.x < p.getX()) {
+        	System.out.println("-1이래");
+            return -1;
+        }
+        else if(this.x == p.getX()) {
+            if(this.y > p.getY()) {
+            	System.out.println("1이래");
+                return 1;
+            }
+            else if(this.y < p.getY()) {
+            	System.out.println("-1이래");
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+}
+
 public class Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-	public static void main(String[] args) {
-		
-		System.out.println("im".compareTo("it"));
-		System.out.println("it".compareTo("im"));
-		System.out.println("it".compareTo("no"));
+        int input = Integer.parseInt(br.readLine());
+        int []x = new int[input];
+        int []y = new int[input];
 
-	}
+        for(int i = 0; i < input; i++) {
+            st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
 
+        Points []points = sortPoints(x, y);
+
+        for(int i = 0; i < input; i++) {
+            sb.append(points[i].getX() + " " + points[i].getY() + "\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+
+    // 힙정렬 사용
+    static Points []sortPoints(int []x, int []y) {
+        PriorityQueue<Points> pointsHeap = new PriorityQueue<>();
+        
+        int count = x.length;
+        Points [] points = new Points[count];
+
+        for(int i = 0; i < count; i++) {
+            pointsHeap.add(new Points(x[i], y[i]));
+        }
+
+        for(int i = 0; i < count; i++) {
+            points[i] = pointsHeap.poll();
+        }
+
+        return points;
+    }
 }
