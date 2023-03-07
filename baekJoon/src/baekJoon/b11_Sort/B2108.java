f2211d2de8b561f1c064febcbf419dc180d7b6bf
booimym <booimym@naver.com>
2023-03-07 18:12:39 +0900

    .

--- baekJoon/src/baekJoon/b11_Sort/B2108.java
+++ baekJoon/src/baekJoon/b11_Sort/B2108.java
@@ -20,6 +20,8 @@ public class B2108 {
 		 * 
 		 * */
 		
+		int average = 0; 	// (1) 산술평균
+		int medium = 0;		// (2) 중앙값
 		
 		// 정렬되기 전 배열 만들기
 		// (1) 배열 길이
@@ -29,11 +31,25 @@ public class B2108 {
 		// (3) 배열 초기화
 		for(int i = 0 ; i < arr.length ; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			average += arr[i];
+			//불필요...왜냐면 5/2하면 되니까...
+			if(i == arr.length/2) {
+				medium = arr[i];		// (2) 중앙값
+			}
 		}
+		
+		average = average / arr.length; // (1) 산술평균
 		
 		//---------------------------------------------------------------------
 		//입력되는 정수의 절댓값은 4,000을 넘지 않는다.
 		int[] counting = new int[8001];
+		
+		for(int i = 0 ; i < arr.length ; i++) {
+			
+			// 음수일 경우도 배열에 넣어지도록.... 4000을 더해준다!
+			counting[arr[i]+4000]++;
+			
+		}
 		
 		
 		
--- baekJoon/src/baekJoon/b11_Sort/B2587.java
+++ baekJoon/src/baekJoon/b11_Sort/B2587.java
@@ -4,28 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 	- 산술평균 : N개의 수들의 합을 N으로 나눈 값
+	- 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+	- 최빈값 : N개의 수들 중 가장 많이 나타나는 값
+	- 범위 : N개의 수들 중 최댓값과 최솟값의 차이
+ *
+ */
 public class B2587 {
 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int average = 0;
-		int medium = 0;
+		int average = 0;	// 산술평균
+		int medium = 0;		// 중앙값
+		
 		for(int i = 0 ; i < 5 ; i++) {
 			int num = Integer.parseInt(br.readLine());
 			average += num;
 			//불필요...왜냐면 5/2하면 되니까...
 			if(i == 2) {
-				medium = num;
+				medium = num; // 중앙값
 			}
 		}
 		
-		average = average/5;
-		//평균
-		System.out.println(average);
-		//중앙값
-		System.out.println(medium);
+		int count = 0; 		// 최빈값
+		
+		average = average/5; // 산술평균
+		
+		// ----------------------------------------------------------------------------------------
+		
+		
 		
 	}
 }
