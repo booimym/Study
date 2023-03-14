3d0d95852559fea36801ca1c653298a78f5b6508
booimym <booimym@naver.com>
2023-03-14 20:08:38 +0900

    ./

--- baekJoon/src/baekJoon/b11_Sort/B10814.java
+++ baekJoon/src/baekJoon/b11_Sort/B10814.java
@@ -18,25 +18,33 @@ public class B10814 {
 	public static void insertSort(String[][] arr) {
 		
 		for(int i = 1 ; i < arr.length ; i++) {
-			String[] temp = arr[i];
+			//String[] temp = arr[i];
+			String temp_1 = arr[i][0];
+			String temp_2 = arr[i][1];
 			int now = Integer.parseInt(arr[i][0]);
 			int index = i-1;
 			
 			// index가 0이상이어야하는데 그건 안 써도 돼?
 			
+			
+			//▲ &&의 앞과 뒤를 바꾸니 해결! 컴퓨터는 멍충했다!
 			//while(Integer.parseInt(arr[index][0]) > now && index >= 0) {
-			while((index >= 0) && (Integer.parseInt(arr[index][0]) > Integer.parseInt(temp[0]) )) {
-				//------------------------------
-				//arr[index+1][0] = arr[index][0];
-				//arr[index+1][1] = arr[index][1];
-				//------------------------------
+			while((index >= 0) && (Integer.parseInt(arr[index][0]) > Integer.parseInt(temp_1) )) {
 				
+				// 물어보기!!!!! 왜 이거랑
+				//------------------------------
+				arr[index+1][0] = arr[index][0];
+				arr[index+1][1] = arr[index][1];
+				//------------------------------
+				// 이거랑 다르게 나와???????????????????????????????????????????????????????????????????
 				//arr[index+1] = arr[index];
 
 				index--;
 			}
 			// while문을 나온 후에, 값 집어넣기
-			arr[index+1] = temp;
+			//arr[index+1] = temp;
+			arr[index+1][0] = temp_1;
+			arr[index+1][1] = temp_2;
 			
 			
 		}
@@ -48,7 +56,7 @@ public class B10814 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		//map 길이
+		//길이 입력받기
 		int length = Integer.parseInt(br.readLine());
 		
 		String[][] arr = new String[length][2];
@@ -57,10 +65,18 @@ public class B10814 {
 			
 			//이러면 바로 0과 1로 나눠지나?
 			arr[i] = br.readLine().split(" ");
-//			String[] a = br.readLine().split(" ");
-//			arr[i][0] = a[0];
-//			arr[i][1] = a[1];
+			
+
 		}
+		
+		
+		System.out.println("배열에 정상적으로 들어갔는지 확인중");
+		for(int i = 0; i < length ; i++) {
+			
+			System.out.println(arr[i][0]+" "+arr[i][1]);
+		}
+		System.out.println("배열에 정상적으로 들어갔는지 확인중");
+
 		
 		//삽입으로 풀어보자!
 		insertSort(arr);
---
+++ baekJoon/src/baekJoon/b11_Sort/B10814_Test2.java
@@ -0,0 +1,115 @@
+package baekJoon.b11_Sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B10814_Test2 {
+
+   // 멤버 변수 선언
+   public static String member[][];
+   public static String temp [][];
+
+   public static void main(String[] args) throws Exception {
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+      // 입력 받을 원소 갯수 인덱스 길이 N 입력 받기
+      int inputN = Integer.parseInt(br.readLine());
+
+      // 2차원 배열 입력받은 인덱스 길이로 초기화
+      member = new String[inputN][2];
+
+      // 2차원 배열에 원소 입력 받기
+      for (int i = 0; i < inputN; i++) {
+         String[] xyNum = br.readLine().split(" ");
+         member[i][0] = xyNum[0];
+         member[i][1] = xyNum[1];
+      }
+
+      temp = new String[member.length][2];
+      // 병합정렬 Top-Down
+      mergeSort(0, member.length - 1);
+
+      // BufferedWriter에 출력 문 담기
+      for (int i = 0; i < inputN; i++) {
+         bw.write(member[i][0] + " " + member[i][1] + "\n");
+      }
+      // 출력 후 스트림 닫기
+      bw.close();
+
+   }
+
+   private static void mergeSort(int left, int right) {
+      // 원소가 1개 일시 실행 안함
+      if (left == right) {
+         return;
+      }
+
+      // 가운대 위치
+      int mid = (left + right) / 2;
+
+      // 절반 중 왼쪽 부분리스트(left ~ mid)
+      mergeSort(left, mid);
+      // 절반 중 오른쪽 부분리스트(mid+1 ~ right)
+      mergeSort(mid + 1, right);
+      // 병합작업
+      merge(left, mid, right);
+
+   }
+
+   private static void merge(int left, int mid, int right) {
+      int mergeLeft = left; 
+      int mergeRight = mid+1; 
+      int idx = left; 
+      
+      while (mergeLeft<=mid  && mergeRight<=right) {
+         
+         if (Integer.parseInt(member[mergeLeft][0])<=Integer.parseInt(member[mergeRight][0])) {
+            
+        	temp[idx] = member[mergeLeft];
+            System.out.println("------------------------------");
+            System.out.println("temp[idx]는 "+ temp[idx]);
+            System.out.println("------------------------------");
+            mergeLeft++;
+            idx++;
+           
+            
+         } else  {
+        	 temp[idx] = member[mergeRight];
+             temp[idx][1] = member[mergeRight][1];
+             System.out.println("------------------------------");
+             System.out.println("temp[idx]는 "+ temp[idx]);
+             System.out.println("------------------------------");
+            mergeRight++;
+            idx++;
+         }
+      }
+      
+      if (mergeLeft>mid) {
+         while (mergeRight<=right) {
+        	 temp[idx] = member[mergeRight];
+        	 System.out.println("------------------------------");
+             System.out.println("temp[idx]는 "+ temp[idx]);
+             System.out.println("------------------------------");
+            idx++;
+            mergeRight++;
+         }
+      }else {
+         while (mergeLeft<=mid) {
+        	 temp[idx] = member[mergeLeft];
+        	 System.out.println("------------------------------");
+             System.out.println("temp[idx]는 "+ temp[idx]);
+             System.out.println("------------------------------");
+            idx++;
+            mergeLeft++;
+         }
+      }
+      for (int i = left; i <= right; i++) {
+         member[i]=temp[i];
+      }
+   }
+
+}
