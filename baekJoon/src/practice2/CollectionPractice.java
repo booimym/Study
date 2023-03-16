223d34b714f4e8ddf592355ca3764744dc9d9b53
booimym <booimym@naver.com>
2023-03-16 16:16:45 +0900

    Create CollectionPractice.java

---
+++ baekJoon/src/practice2/CollectionPractice.java
@@ -0,0 +1,22 @@
+package practice2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollectionPractice {
+
+	public static void main(String[] args) {
+		
+		int[] arr1 = {1,2,3,4,5};
+		
+		ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(arr1));
+
+		String[] strs = {"alpha", "beta", "charlie"};
+		List<String> lst = Arrays.asList(strs); 
+		
+		//------------------------------------
+		 List<String> e2st = new ArrayList<String>(Arrays.asList(strs)); 
+	}
+
+}
