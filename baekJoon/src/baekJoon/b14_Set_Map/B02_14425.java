0fd5048eb04920feda006fc3476f537ffdcca894
booimym <booimym@naver.com>
2023-03-28 19:30:11 +0900

    .

---
+++ baekJoon/src/baekJoon/b14_Set_Map/B02_14425.java
@@ -0,0 +1,26 @@
+package baekJoon.b14_Set_Map;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * [ 문자열 집합 ]
+ *
+ */
+public class B02_14425 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String[] input = br.readLine().split(" ");
+		
+		
+		
+	}
+
+}
